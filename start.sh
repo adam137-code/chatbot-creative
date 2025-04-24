a4070c9ab32fbed7a71781ecd9e917057e0c94be
Adam Wahana <60256064+adam137-code@users.noreply.github.com>
2025-04-24 14:38:37 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install dependencies yang diperlukan
+apt-get update && apt-get install -y \
+  libgconf-2-4 \
+  libnss3 \
+  libxss1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libgtk-3-0 \
+  libdrm2 \
+  xdg-utils \
+  fonts-liberation \
+  libappindicator3-1 \
+  libnspr4 \
+  libnss3 \
+  libx11-xcb1
+
+# Install npm packages dan jalankan bot
+npm install
+node index.js
