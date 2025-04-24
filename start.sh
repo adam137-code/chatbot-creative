df7fc65f53b78b6b493f7219fc9c406222602f45
Adam Wahana <60256064+adam137-code@users.noreply.github.com>
2025-04-24 19:54:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Install dependencies yang diperlukan
-apt-get update && apt-get install -y \
-  libgconf-2-4 \
-  libnss3 \
-  libxss1 \
-  libasound2 \
-  libatk-bridge2.0-0 \
-  libgtk-3-0 \
-  libdrm2 \
-  xdg-utils \
+RUN apt-get update && apt-get install -y \
+  chromium \
   fonts-liberation \
   libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libgdk-pixbuf2.0-0 \
   libnspr4 \
   libnss3 \
-  libx11-xcb1
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils \
+  --no-install-recommends
 
 # Install npm packages dan jalankan bot
 npm install
